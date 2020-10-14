213569ec1fa2fc1647bdf9d5065e43022d16b800
Artur Khasbiev <Khasbiich@yandex.ru>
2020-10-15 01:23:44 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,27 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let letters = [];
+    let newArr = [];
+
+    for (let i = 0; i < expr.length; i += 10){
+      letters.push(expr.substring(i,i + 10).replace(/^0+/, ''));
+    }
+      for(i = 0; i < letters.length; i++ ){
+          if (letters[i] === '**********'){
+          letters[i] = ' ';
+          newArr.push(letters[i]);
+        } else {
+        letters[i] = letters[i].replace(/11/g , '-').replace(/10/g , '.');
+        newArr.push(MORSE_TABLE[letters[i]]);
+        }
+      }
+
+
+      for(i = 0; i < letters.length; i++ ){
+        console.log();
+      }
+    return newArr.join('');
 }
 
 module.exports = {
